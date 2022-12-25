699fb031137a03ab8d0611f64fba6e8e21145b6a
camjac251 <camjac251@users.noreply.github.com>
2022-12-25 17:50:32 -0600

    optimize and add new functionality

--- plugins/admin/punishment.lua
+++ plugins/admin/punishment.lua
@@ -1,13 +1,13 @@
 ---@type Plugin
 local plugin = ...
 
-local shared = plugin:require('shared')
-local persistence = plugin:require('persistence')
+local shared = plugin:require("shared")
+local persistence = plugin:require("persistence")
 
 ---@param minutes integer
 ---@return string
-local function getCleanReadableTime (minutes)
-	local str = ''
+local function getCleanReadableTime(minutes)
+	local str = ""
 
 	local hours = math.floor(minutes / 60)
 	if hours > 0 then
@@ -17,72 +17,83 @@ local function getCleanReadableTime (minutes)
 		if days > 0 then
 			hours = hours - (days * 24)
 
-			str = str .. days .. 'd '
+			str = str .. days .. "d "
 		end
 
 		if hours > 0 then
-			str = str .. hours .. 'h '
+			str = str .. hours .. "h "
 		end
 	end
 
 	if minutes > 0 then
-		str = str .. minutes .. 'm'
+		str = str .. minutes .. "m"
 	end
 
 	return str
 end
 
-plugin.commands['/kick'] = {
-	info = 'Kick a player.',
-	usage = '<phoneNumber/name> [reason]',
-	canCall = function (ply) return ply.isConsole or isModeratorOrAdmin(ply) end,
+plugin.commands["/kick"] = {
+	info = "Kick a player.",
+	usage = "<phoneNumber/name> [reason]",
+	canCall = function(ply)
+		return ply.isConsole or isModeratorOrAdmin(ply)
+	end,
 	autoComplete = shared.autoCompletePlayerFirstArg,
 	---@param ply Player
 	---@param args string[]
-	call = function (ply, _, args)
-		assert(#args >= 1, 'usage')
+	call = function(ply, _, args)
+		assert(#args >= 1, "usage")
 
 		local victim = findOnePlayer(table.remove(args, 1))
-		if not victim.connection then error('Player not connected') end
+		if not victim.connection then
+			error("Player not connected")
+		end
 
-		local reason = #args > 0 and table.concat(args, ' ') or 'No reason specified.'
+		local reason = #args > 0 and table.concat(args, " ") or "No reason specified."
 
 		victim.connection.timeoutTime = 50 * server.TPS
 
-		adminLog('%s kicked %s (%s), reason: %s', ply.name, victim.name, dashPhoneNumber(victim.phoneNumber), reason)
+		adminLog("%s kicked %s (%s), reason: %s", ply.name, victim.name, dashPhoneNumber(victim.phoneNumber), reason)
 
 		shared.discordEmbed({
-			title = 'Player Kicked',
+			title = "Player Kicked",
 			color = 0xF57C00,
-			description = string.format('**%s** kicked **%s** (%s)', ply.name, victim.name, dashPhoneNumber(victim.phoneNumber)),
+			description = string.format(
+				"**%s** kicked **%s** (%s)",
+				ply.name,
+				victim.name,
+				dashPhoneNumber(victim.phoneNumber)
+			),
 			fields = {
 				{
-					name = 'Reason',
-					value = reason
-				}
-			}
+					name = "Reason",
+					value = reason,
+				},
+			},
 		})
-	end
+	end,
 }
 
-plugin.commands['/punish'] = {
-	info = 'Ban an account based on previous bans.',
-	usage = '<phoneNumber/name> [reason] [count]',
-	canCall = function (ply) return ply.isConsole or isModeratorOrAdmin(ply) end,
+plugin.commands["/punish"] = {
+	info = "Ban an account based on previous bans.",
+	usage = "<phoneNumber/name> [reason] [count]",
+	canCall = function(ply)
+		return ply.isConsole or isModeratorOrAdmin(ply)
+	end,
 	autoComplete = shared.autoCompleteAccountFirstArg,
 	---@param ply Player
 	---@param args string[]
-	call = function (ply, _, args)
-		assert(#args >= 1, 'usage')
+	call = function(ply, _, args)
+		assert(#args >= 1, "usage")
 
 		local acc = findOneAccount(args[1])
 
 		local phoneString = tostring(acc.phoneNumber)
-		local reason = args[2] or 'No reason specified.'
+		local reason = args[2] or "No reason specified."
 
-		local count = assert(tonumber(args[3] or 1), 'Count is not a number')
+		local count = assert(tonumber(args[3] or 1), "Count is not a number")
 		count = math.floor(count)
-		assert(count >= 1, 'Invalid count')
+		assert(count >= 1, "Invalid count")
 
 		local persistentData = persistence.get()
 
@@ -99,26 +110,34 @@ plugin.commands['/punish'] = {
 		end
 
 		table.insert(persistentData.warnings[phoneString], {
-			reason = 'Banned for ' .. getCleanReadableTime(banMinutes) .. ': ' .. reason,
-			time = os.time()
+			reason = "Banned for " .. getCleanReadableTime(banMinutes) .. ": " .. reason,
+			time = os.time(),
 		})
 
 		persistence.save()
 
 		acc.banTime = acc.banTime + banMinutes
 
-		adminLog('%s punished %s (%s) @ %im (x%d), reason: %s', ply.name, acc.name, dashPhoneNumber(acc.phoneNumber), banMinutes, count, reason)
+		adminLog(
+			"%s punished %s (%s) @ %im (x%d), reason: %s",
+			ply.name,
+			acc.name,
+			dashPhoneNumber(acc.phoneNumber),
+			banMinutes,
+			count,
+			reason
+		)
 
-		local countString = 'a punishment'
+		local countString = "a punishment"
 		if count > 1 then
-			countString = count .. ' punishments'
+			countString = count .. " punishments"
 		end
 
 		shared.discordEmbed({
-			title = 'Player Banned',
+			title = "Player Banned",
 			color = 0xD32F2F,
 			description = string.format(
-				'**%s** added %s to **%s** (%s), ban set at **%s**',
+				"**%s** added %s to **%s** (%s), ban set at **%s**",
 				ply.name,
 				countString,
 				acc.name,
@@ -127,37 +146,42 @@ plugin.commands['/punish'] = {
 			),
 			fields = {
 				{
-					name = 'Reason',
-					value = reason
-				}
-			}
+					name = "Reason",
+					value = reason,
+				},
+			},
 		})
-	end
+	end,
 }
 
-plugin.commands['/unpunish'] = {
-	info = 'Remove punishments from an account.',
-	usage = '<phoneNumber/name> [reason] [count]',
-	canCall = function (ply) return ply.isConsole or isModeratorOrAdmin(ply) end,
+plugin.commands["/unpunish"] = {
+	info = "Remove punishments from an account.",
+	usage = "<phoneNumber/name> [reason] [count]",
+	canCall = function(ply)
+		return ply.isConsole or isModeratorOrAdmin(ply)
+	end,
 	autoComplete = shared.autoCompleteAccountFirstArg,
 	---@param ply Player
 	---@param args string[]
-	call = function (ply, _, args)
-		assert(#args >= 1, 'usage')
+	call = function(ply, _, args)
+		assert(#args >= 1, "usage")
 
 		local acc = findOneAccount(args[1])
 
 		local phoneString = tostring(acc.phoneNumber)
-		local reason = args[2] or 'No reason specified.'
+		local reason = args[2] or "No reason specified."
 
-		local count = assert(tonumber(args[3] or 1), 'Count is not a number')
+		local count = assert(tonumber(args[3] or 1), "Count is not a number")
 		count = math.floor(count)
-		assert(count >= 1, 'Invalid count')
+		assert(count >= 1, "Invalid count")
 
 		local persistentData = persistence.get()
 
-		assert(persistentData.punishments[phoneString] and persistentData.punishments[phoneString] >= 1, 'Account has no punishments')
-		assert(persistentData.punishments[phoneString] >= count, 'Count is too high')
+		assert(
+			persistentData.punishments[phoneString] and persistentData.punishments[phoneString] >= 1,
+			"Account has no punishments"
+		)
+		assert(persistentData.punishments[phoneString] >= count, "Count is too high")
 
 		local banMinutes = 0
 		for _ = 1, count do
@@ -169,18 +193,26 @@ plugin.commands['/unpunish'] = {
 
 		acc.banTime = math.max(0, acc.banTime - banMinutes)
 
-		adminLog('%s unpunished %s (%s) @ %im (x%d), reason: %s', ply.name, acc.name, dashPhoneNumber(acc.phoneNumber), banMinutes, count, reason)
+		adminLog(
+			"%s unpunished %s (%s) @ %im (x%d), reason: %s",
+			ply.name,
+			acc.name,
+			dashPhoneNumber(acc.phoneNumber),
+			banMinutes,
+			count,
+			reason
+		)
 
-		local countString = 'a punishment'
+		local countString = "a punishment"
 		if count > 1 then
-			countString = count .. ' punishments'
+			countString = count .. " punishments"
 		end
 
 		shared.discordEmbed({
-			title = 'Player Unbanned',
+			title = "Player Unbanned",
 			color = 0x388E3C,
 			description = string.format(
-				'**%s** removed %s from **%s** (%s), deducted **%s**',
+				"**%s** removed %s from **%s** (%s), deducted **%s**",
 				ply.name,
 				countString,
 				acc.name,
@@ -189,40 +221,94 @@ plugin.commands['/unpunish'] = {
 			),
 			fields = {
 				{
-					name = 'Reason',
-					value = reason
-				}
-			}
+					name = "Reason",
+					value = reason,
+				},
+			},
 		})
-	end
+	end,
 }
 
-plugin.commands['/ban'] = {
-	info = 'Ban an account.',
-	usage = '<phoneNumber/name> <minutes> [reason]',
-	canCall = function (ply) return ply.isConsole or ply.isAdmin end,
+plugin.commands["/ban"] = {
+	info = "Ban an account.",
+	usage = "<phoneNumber/name> [minutes] [reason]",
+	canCall = function(ply)
+		return ply.isConsole or ply.isAdmin
+	end,
 	autoComplete = shared.autoCompleteAccountFirstArg,
 	---@param ply Player
 	---@param args string[]
-	call = function (ply, _, args)
-		assert(#args >= 2, 'usage')
+	call = function(ply, _, args)
+		assert(#args >= 1, "usage")
+		local acc = findOneAccount(args[1])
+		local banTime = args[2]
+		local reason
+		-- if banTime ends in a "m", remove it, and convert to minutes else if ends in a "h", convert to minutes else if banTime is p or permanent, set banTime to 0
+		if banTime then
+			-- if banTime doesn't end in a m h d w or permanent, assume the time is 0 and output reason
+			if
+				banTime:sub(-1) ~= "m"
+				and banTime:sub(-1) ~= "h"
+				and banTime:sub(-1) ~= "d"
+				and banTime:sub(-1) ~= "w"
+				and banTime:sub(-1) ~= "p"
+			then
+				reason = banTime
+				banTime = 0
+			else
+				if banTime:sub(-1) == "m" then
+					banTime = tonumber(banTime:sub(1, -2))
+				elseif banTime:sub(-1) == "h" then
+					banTime = tonumber(banTime:sub(1, -2)) * 60
+				elseif banTime:sub(-1) == "d" then
+					banTime = tonumber(banTime:sub(1, -2)) * 60 * 24
+				elseif banTime:sub(-1) == "w" then
+					banTime = tonumber(banTime:sub(1, -2)) * 60 * 24 * 7
+				elseif banTime:sub(-1) == "y" then
+					banTime = tonumber(banTime:sub(1, -2)) * 60 * 24 * 365
+				elseif banTime:sub(-1) == ("p" or "perm" or "permanent") then
+					banTime = 0
+				end
+				assert(tonumber(banTime), "Ban time is not a number")
+				reason = args[3] or "No reason specified."
+			end
+		else
+			reason = args[2] or "No reason specified."
+			banTime = 0
+		end
 
-		local acc = findOneAccount(table.remove(args, 1))
+		local phoneString = tostring(acc.phoneNumber)
 
-		local banTime = math.floor(table.remove(args, 1) or 0)
-		if banTime < 1 then error('Invalid ban time') end
-
-		local reason = #args > 0 and table.concat(args, ' ') or 'No reason specified.'
+		local reason = #args > 0 and table.concat(args, " ") or "No reason specified."
 
 		acc.banTime = banTime
 
-		adminLog('%s banned %s (%s) for %imin, reason: %s', ply.name, acc.name, dashPhoneNumber(acc.phoneNumber), banTime, reason)
+		local persistentData = persistence.get()
+
+		if not persistentData.bans[phoneString] then
+			persistentData.bans[phoneString] = {}
+		end
+
+		table.insert(persistentData.bans[phoneString], {
+			reason = reason,
+			time = os.time(),
+		})
+		persistence.save()
+
+		adminLog(
+			"%s banned %s (%s) for %imin, reason: %s",
+			ply.name,
+			acc.name,
+			dashPhoneNumber(acc.phoneNumber),
+			banTime,
+			reason
+		)
 
 		shared.discordEmbed({
-			title = 'Player Banned',
+			title = "Player Banned",
 			color = 0xD32F2F,
 			description = string.format(
-				'**%s** manually banned **%s** (%s) for **%s**',
+				"**%s** manually banned **%s** (%s) for **%s**",
 				ply.name,
 				acc.name,
 				dashPhoneNumber(acc.phoneNumber),
@@ -230,63 +316,72 @@ plugin.commands['/ban'] = {
 			),
 			fields = {
 				{
-					name = 'Reason',
-					value = reason
-				}
-			}
+					name = "Reason",
+					value = reason,
+				},
+			},
 		})
-	end
+	end,
 }
 
-plugin.commands['/unban'] = {
-	info = 'Unban an account.',
-	usage = '<phoneNumber/name> [reason]',
-	canCall = function (ply) return ply.isConsole or ply.isAdmin end,
+plugin.commands["/unban"] = {
+	info = "Unban an account.",
+	usage = "<phoneNumber/name> [reason]",
+	canCall = function(ply)
+		return ply.isConsole or ply.isAdmin
+	end,
 	autoComplete = shared.autoCompleteAccountFirstArg,
 	---@param ply Player
 	---@param args string[]
-	call = function (ply, _, args)
-		assert(#args >= 1, 'usage')
+	call = function(ply, _, args)
+		assert(#args >= 1, "usage")
 
 		local acc = findOneAccount(table.remove(args, 1))
 
-		local reason = #args > 0 and table.concat(args, ' ') or 'No reason specified.'
+		local reason = #args > 0 and table.concat(args, " ") or "No reason specified."
 
 		acc.banTime = 0
 
-		adminLog('%s unbanned %s (%s), reason: %s', ply.name, acc.name, dashPhoneNumber(acc.phoneNumber), reason)
+		adminLog("%s unbanned %s (%s), reason: %s", ply.name, acc.name, dashPhoneNumber(acc.phoneNumber), reason)
 
 		shared.discordEmbed({
-			title = 'Player Unbanned',
+			title = "Player Unbanned",
 			color = 0x388E3C,
-			description = string.format('**%s** manually unbanned %s (%s)', ply.name, acc.name, dashPhoneNumber(acc.phoneNumber)),
+			description = string.format(
+				"**%s** manually unbanned %s (%s)",
+				ply.name,
+				acc.name,
+				dashPhoneNumber(acc.phoneNumber)
+			),
 			fields = {
 				{
-					name = 'Reason',
-					value = reason
-				}
-			}
+					name = "Reason",
+					value = reason,
+				},
+			},
 		})
-	end
+	end,
 }
 
-plugin.commands['/kill'] = {
-	info = 'Kill a player.',
-	usage = '<phoneNumber/name>',
-	canCall = function (ply) return ply.isConsole or ply.isAdmin end,
+plugin.commands["/kill"] = {
+	info = "Kill a player.",
+	usage = "<phoneNumber/name>",
+	canCall = function(ply)
+		return ply.isConsole or ply.isAdmin
+	end,
 	autoComplete = shared.autoCompletePlayerFirstArg,
 	---@param ply Player
 	---@param args string[]
-	call = function (ply, _, args)
-		assert(#args >= 1, 'usage')
+	call = function(ply, _, args)
+		assert(#args >= 1, "usage")
 
 		local victim = findOnePlayer(table.remove(args, 1))
 
 		local victimMan = victim.human
-		assert(victimMan, 'Victim not spawned in')
+		assert(victimMan, "Victim not spawned in")
 
 		victimMan.isAlive = false
 
-		adminLog('%s killed %s (%s)', ply.name, victim.name, dashPhoneNumber(victim.phoneNumber))
-	end
+		adminLog("%s killed %s (%s)", ply.name, victim.name, dashPhoneNumber(victim.phoneNumber))
+	end,
 }
--- plugins/admin/teleportation.lua
+++ plugins/admin/teleportation.lua
@@ -1,105 +1,212 @@
 ---@type Plugin
 local plugin = ...
 
-local function moveItem(item, difference)
+function MoveItem(item, difference)
 	item.pos:add(difference)
 	item.rigidBody.pos:add(difference)
 end
 
-local function teleportHumanWithItems(man, pos)
+function TeleportHumanWithItems(man, pos)
+	for i2 = 0, 15 do
+		local rb = man:getRigidBody(i2)
+		if rb.data.isGrabbing then
+			rb.data.isGrabbing = nil
+		end
+		if rb.data.bond ~= nil then
+			rb.data.bond:remove()
+			rb.data.bond = nil
+		end
+	end
 	local oldPos = man.pos:clone()
 	oldPos:mult(-1.0)
 	local difference = pos:clone()
 	difference:add(oldPos)
 
 	man:teleport(pos)
+	man:setVelocity(Vector(0, 0, 0))
 	for i = 0, 1 do
 		local item = man:getInventorySlot(i).primaryItem
 		if item then
-			moveItem(item, difference)
+			MoveItem(item, difference)
 		end
 	end
 end
 
-plugin.commands['/find'] = {
-	info = 'Teleport to a player.',
-	usage = '<phoneNumber/name>',
-	canCall = function (ply) return ply.isAdmin end,
+plugin.commands["/find"] = {
+	info = "Teleport to a player.",
+	usage = "<phoneNumber/name>",
+	canCall = function(ply)
+		return ply.isAdmin
+	end,
 	---@param ply Player
 	---@param man Human?
 	---@param args string[]
-	call = function (ply, man, args)
-		assert(#args >= 1, 'usage')
-		assert(man, 'Not spawned in')
+	call = function(ply, man, args)
+		assert(#args >= 1, "usage")
+		assert(man, "Not spawned in")
 
 		local victim = findOnePlayer(table.remove(args, 1))
-
 		local victimMan = victim.human
-		assert(victimMan, 'Victim not spawned in')
+		assert(victimMan, "Victim not spawned in")
 
 		-- Forward yaw plus 180 degrees
-		local yaw = victimMan.viewYaw + math.pi/2
+		local yaw = victimMan.viewYaw + math.pi / 2
 		local distance = 3
 
 		local pos = victimMan.pos:clone()
 		pos.x = pos.x + (distance * math.cos(yaw))
 		pos.z = pos.z + (distance * math.sin(yaw))
 
-		teleportHumanWithItems(man, pos)
+		if man.vehicle ~= nil then
+			man.vehicle = nil
+		end
+		TeleportHumanWithItems(man, pos)
 
-		adminLog('%s found %s (%s)', ply.name, victim.name, dashPhoneNumber(victim.phoneNumber))
-	end
+		if victimMan.vehicle ~= nil then
+			man.vehicle = victimMan.vehicle
+			man.vehicleSeat = 3
+		end
+
+		adminLog("%s found %s (%s)", ply.name, victim.name, dashPhoneNumber(victim.phoneNumber))
+	end,
 }
 
-plugin.commands['/fetch'] = {
-	info = 'Teleport a player to you.',
-	usage = '<phoneNumber/name>',
-	canCall = function (ply) return ply.isAdmin end,
+plugin.commands["/fetch"] = {
+	info = "Teleport a player to you.",
+	usage = "<phoneNumber/name>",
+	canCall = function(ply)
+		return ply.isAdmin
+	end,
 	---@param ply Player
 	---@param man Human?
 	---@param args string[]
-	call = function (ply, man, args)
-		assert(#args >= 1, 'usage')
-		assert(man, 'Not spawned in')
+	call = function(ply, man, args)
+		assert(#args >= 1, "usage")
+		assert(man, "Not spawned in")
 
 		local victim = findOnePlayer(table.remove(args, 1))
 
 		local victimMan = victim.human
-		assert(victimMan, 'Victim not spawned in')
+		assert(victimMan, "Victim not spawned in")
 
 		-- Forward yaw
-		local yaw = man.viewYaw - math.pi/2
+		local yaw = man.viewYaw - math.pi / 2
 		local distance = 3
 
 		local pos = man.pos:clone()
 		pos.x = pos.x + (distance * math.cos(yaw))
 		pos.z = pos.z + (distance * math.sin(yaw))
 
-		teleportHumanWithItems(victimMan, pos)
+		if victimMan.vehicle ~= nil then
+			victimMan.vehicle = nil
+		end
 
-		adminLog('%s fetched %s (%s)', ply.name, victim.name, dashPhoneNumber(victim.phoneNumber))
-	end
+		TeleportHumanWithItems(victimMan, pos)
+		if man.vehicle ~= nil then
+			victimMan.vehicle = man.vehicle
+			victimMan.vehicleSeat = 3
+		end
+		adminLog("%s fetched %s (%s)", ply.name, victim.name, dashPhoneNumber(victim.phoneNumber))
+	end,
 }
 
-plugin.commands['/hide'] = {
-	info = 'Teleport to an inaccessible room.',
-	canCall = function (ply) return ply.isAdmin end,
+plugin.commands["/teleport"] = {
+	info = "TP",
+	alias = { "/tp" },
+
+	usage = "<phoneNumber/name> <phoneNumber/name>",
+	canCall = function(ply)
+		return ply.isAdmin
+	end,
+	autoComplete = autoCompleteAccountFirstArg,
+	---@param ply Player
+	---@param args string[]
+
+	call = function(ply, _, args)
+		assert(#args >= 1, "usage")
+		local p1 = findOnePlayer(args[1])
+		local p2 = findOnePlayer(args[2])
+		local player1 = p1.human
+		local player2 = p2.human
+
+		assert(player1, p1.name .. " is not spawned in")
+		assert(player2, p1.name .. " is not spawned in")
+
+		-- Forward yaw
+		local yaw = player2.viewYaw - math.pi / 2
+		local distance = 3
+
+		local pos = player2.pos:clone()
+		pos.x = pos.x + (distance * math.cos(yaw))
+		pos.z = pos.z + (distance * math.sin(yaw))
+
+		if player1.vehicle ~= nil then
+			player1.vehicle = nil
+		end
+
+		if player2.vehicle ~= nil then
+			player1.vehicle = player2.vehicle
+		end
+
+		TeleportHumanWithItems(player1, pos)
+	end,
+}
+
+plugin.commands["/hide"] = {
+	info = "Teleport to an inaccessible room.",
+	canCall = function(ply)
+		return ply.isAdmin
+	end,
 	---@param ply Player
 	---@param man Human?
-	call = function (ply, man)
-		assert(man, 'Not spawned in')
+	call = function(ply, man)
+		assert(man, "Not spawned in")
 
 		local level = server.loadedLevel
 		local pos
 
-		if level == 'test2' then
+		if level == "test2" then
 			pos = Vector(1505, 33.1, 1315)
 		else
-			error('Unsupported map')
+			error("Unsupported map")
 		end
 
-		teleportHumanWithItems(man, pos)
+		TeleportHumanWithItems(man, pos)
 
-		adminLog('%s hid', ply.name)
-	end
+		adminLog("%s hid", ply.name)
+	end,
+}
+
+plugin.commands["/go"] = {
+	info = "Go to where you are pointing.",
+	canCall = function(ply)
+		return ply.isConsole or isModeratorOrAdmin(ply)
+	end,
+	---@param ply Player
+	---@param man Human?
+	call = function(ply, man)
+		assert(man, "Not spawned in")
+
+		local dist = 10000
+
+		local yaw = man.viewYaw - math.pi / 2
+		local pitch = -man.viewPitch
+
+		local pos = man:getRigidBody(3).pos
+		local pos2 = Vector(
+			pos.x + (dist * math.cos(yaw) * math.cos(pitch)),
+			pos.y + (dist * math.sin(pitch)),
+			pos.z + (dist * math.sin(yaw) * math.cos(pitch))
+		)
+
+		local ray = physics.lineIntersectLevel(pos, pos2, false)
+
+		if ray.hit then
+			local NewPos = ray.pos
+			pos.y = pos.y + 2
+			TeleportHumanWithItems(man, NewPos)
+			man:addVelocity(Vector(-0.025, 0, 0))
+		end
+		adminLog("%s used go and went to a new position", ply.name)
+	end,
 }
